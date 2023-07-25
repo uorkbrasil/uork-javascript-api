13855bb76b87a85a607a17972e633af9794f49f7
Matheus <90228238+ofmxtheuuz@users.noreply.github.com>
2023-07-25 16:14:58 -0300

    start.bat
    Add option to install dependencies. By https://github.com/ofmxtheuuz

--- uork-endpoints/start.bat
+++ uork-endpoints/start.bat
@@ -1,13 +1,12 @@
-
-
 @echo off
-title Uork - API Example
+title Uork - API
 setlocal
 
-echo Uork - API Example
-echo 1 - Consultar Status de um servico
+echo Uork - API
+echo 1 - Consultar Status de um serviço
 echo 2 - Consultar Conta
-echo 3 - Consultar Noticias
+echo 3 - Consultar Notícias
+echo 4 - Instalar Dependências
 
 set /p option=Qual a escolha desejada: 
 
@@ -20,9 +19,11 @@ if "%option%"=="1" (
 ) else if "%option%"=="3" (
   cls
   node src/Noticias/app.js
+) else if "%option%"=="4" (
+  cls
+  npm install
 ) else (
     echo Opção inválida.
 )
 
 pause
-
